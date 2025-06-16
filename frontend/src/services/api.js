cda54706a4377f8d5a6e082724e6c6c8d013ea52
bradshirling <bradleyshirling@gmail.com>
2025-06-16 12:40:16 -0500

    second commit

---
+++ frontend/src/services/api.js
@@ -0,0 +1,18 @@
+const API_KEY = "e52d1a53caf97a98382d2e107e9b23a8"
+const BASE_URL = "https://api.themoviedb.org/3"
+
+export const getPopularMovies = async () => {
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`)
+    const data = await response.json()
+    return data.results
+}
+
+export const searchMovies = async (query) => {
+    const response = await fetch(
+        `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+        query
+    )}`
+    )
+    const data = await response.json()
+    return data.results
+}
